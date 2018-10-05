9c5ff325f5a0660ea329c99115934863e6c7e772
Anna-Sh <a.shmidova@yandex.ru>
2018-10-05 17:18:23 +0300

    algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let maxLen = 0;
+    const hashSet = new Set(array);
+
+    for (let num of hashSet){
+        let next = num + 1, localLen = 1;
+
+        if(!hashSet.has(num-1)) {
+            while(hashSet.has(next++)){
+                localLen++;
+            }
+            maxLen = Math.max(maxLen, localLen);
+        }
+    }
+    return maxLen;
+};
